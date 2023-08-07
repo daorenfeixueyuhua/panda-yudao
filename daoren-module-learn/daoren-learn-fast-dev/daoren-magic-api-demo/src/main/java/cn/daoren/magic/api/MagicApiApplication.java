bfa961e8a820b3590c25e94a64bb3c948afe903c
pengda <daorenfeixueyuhua@gmail.com>
2023-08-07 17:39:50 +0800

    feat: magic学习

---
+++ daoren-module-learn/daoren-learn-fast-dev/daoren-magic-api-demo/src/main/java/cn/daoren/magic/api/MagicApiApplication.java
@@ -0,0 +1,15 @@
+package cn.daoren.magic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author peng_da
+ * @since 2023/7/19 9:42
+ */
+@SpringBootApplication
+public class MagicApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MagicApiApplication.class, args);
+    }
+}
