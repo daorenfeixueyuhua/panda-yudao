b6dd2bb1d1c43ba70aadee1dcf9841352835cffa
pengda <daorenfeixueyuhua@gmail.com>
2023-08-07 17:43:06 +0800

    feat: 常用SQL

---
+++ sql/常用SQL.sql
@@ -0,0 +1,19 @@
+# 查看数据库大小
+select table_schema                                 as '数据库',
+       sum(table_rows)                              as '记录数',
+       sum(truncate(data_length / 1024 / 1024, 2))  as '数据容量(MB)',
+       sum(truncate(index_length / 1024 / 1024, 2)) as '索引容量(MB)'
+from information_schema.tables
+group by table_schema
+order by sum(data_length) desc, sum(index_length) desc;
+# 查看表大小
+select table_schema                            as '数据库',
+       table_name                              as '表名',
+       table_rows                              as '记录数',
+       truncate(data_length / 1024 / 1024, 2)  as '数据容量(MB)',
+       truncate(index_length / 1024 / 1024, 2) as '索引容量(MB)'
+from information_schema.tables
+order by data_length desc, index_length desc;
+
+
+
