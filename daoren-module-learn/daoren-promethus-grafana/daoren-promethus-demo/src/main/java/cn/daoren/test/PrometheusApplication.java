c4a7398b58df48bef9f51af72c244f441dd7cb76
pengda <daorenfeixueyuhua@gmail.com>
2023-08-07 17:40:39 +0800

    feat: grafana学习

---
+++ daoren-module-learn/daoren-promethus-grafana/daoren-promethus-demo/src/main/java/cn/daoren/test/PrometheusApplication.java
@@ -0,0 +1,11 @@
+package cn.daoren.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusApplication.class, args);
+    }
+}
